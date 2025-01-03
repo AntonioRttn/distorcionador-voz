99f5cb9527298bf3bcb39b3b40dbd42876b5a813
AntonioRttn <pzepedah1300@alumno.ipn.mx>
2025-01-03 16:43:13 -0600

    Add files via upload
    Actualización para usar el puerto 8080 en Railway

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, send_file
 import wave
 import numpy as np
@@ -55,5 +54,5 @@ def home():
         </form>
     '''
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
